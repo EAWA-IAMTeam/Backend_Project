14a31fc8eb0c852e7868cfc6a503fe51d0c33826
GohKoonLoong <gohkoonloong01@gmail.com>
2025-02-05 18:10:13 +0800

    Update branch orders

--- internal/orders/models/orders.go
+++ internal/orders/models/orders.go
@@ -1,1 +1,12 @@
 package models
+
+import (
+	"backend_project/internal/products/models"
+)
+
+type Order struct {
+	ID           int            `json:"id"`
+	Product      models.Product `json:"product"`
+	Quantity     int            `json:"quantity"`
+	CustomerName string         `json:"customer_name"`
+}
